3fb0c0aacc90ab20bc040859822cc124552221f5
Jack Kenna <jack.kenna@skybettingandgaming.com>
2016-09-17 22:35:03 +0100

    links all working and Spotify API accessed

--- js/app.js
+++ js/app.js
@@ -20,17 +20,17 @@ MN.domElementsSetup = function(){
 };
 
 MN.changePage = function(page) {
-
+	
 	var page = page+"_page";
 
-	var newpg = document.getElementById(page);
+	if(page == "album_page"){
+		this.getApi();
+	}
 
-    if (newpg.style.visibility === 'hidden') {
-        newpg.style.visibility = 'visible';
-    } else {
-        newpg.style.visibility = 'hidden';
-    }
-    
+	var newpg = document.getElementById(page);
+	console.log(newpg);
+	$(newpg).show();
+	$(newpg).siblings().hide();
 }
 
 
@@ -39,10 +39,37 @@ MN.addEventListener = function(){
 	this.lis.forEach(function(li) {
 		li.addEventListener("click", function(){
 			var id = li.getAttribute("id");
-			//clicked ? clicked = true : clicked = false;
 			console.log(id);
 			this.changePage(id);
 		}.bind(this));
 	}.bind(this));
 
+	
+
 }
+
+MN.getApi= function(){
+
+	var accessToken = "e0213fed63364b669aba9116bb89d9d5";
+	$.ajax({
+	   url: "https://api.spotify.com/v1/artists/3CGzpCMqpqHnafmn2PFQd9/albums",
+	   headers: {
+	       'Authorization': 'Bearer ' + accessToken
+	   },
+	   success: function(result) {
+	       $.each(result.items, function(i,item){
+                var artistReturnedResult = result.items;
+                var avalibleMarket = result.items[i].available_markets;
+                if( avalibleMarket.indexOf('GB') > -1 ){
+	                var name = "<h4>Name:</h4> " + result.items[i].name;
+	                var link = "<h4>Spotify Listen:</h4> " + result.items[i].external_urls.spotify;
+	                var img = "<img src="+result.items[i].images[1].url+" height="+result.items[i].images[1].height+" width="+result.items[i].images[1].width+">"
+	               	var nameCol = name + "<br/>" + link + "<br/><br/>"+ img+ "<br/><br/><br/><br/>";
+					$(".artistInfo").append(nameCol); 
+				}
+           })
+	   }
+	});
+
+}
+
