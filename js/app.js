635444ab80a635e45c880880f5b0fde3a722347d
Jack Kenna <jack.kenna@skybettingandgaming.com>
2016-09-17 23:40:51 +0100

    Comments added to JavaScript

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ MN.domElementsSetup = function(){
 //When the page changes 
 MN.changePage = function(page) {
 	
+	//get new page div
 	var page = "#"+page+"_page";
 
 	if(page == "#album_page"){
@@ -36,8 +37,8 @@ MN.changePage = function(page) {
 
 
 MN.addEventListener = function(){
-	
-  $('li').on("click", function(li) {
+	//when an li is clicked get the attribute and send it to changePage
+	$('li').on("click", function(li) {
    		var $id = $(this).attr('id');
 		MN.changePage($id);
     });
@@ -49,21 +50,25 @@ MN.addEventListener = function(){
 
 MN.getApi= function(){
 
+	//asign accessToken 
 	var accessToken = "e0213fed63364b669aba9116bb89d9d5";
+	//use AJAX to get the API
 	$.ajax({
 	   url: "https://api.spotify.com/v1/artists/3CGzpCMqpqHnafmn2PFQd9/albums",
 	   headers: {
 	       'Authorization': 'Bearer ' + accessToken
 	   },
 	   success: function(result) {
-	       $.each(result.items, function(i,item){
+	   		//loop through each object and save the variables
+	   		$.each(result.items, function(i,item){
                 var artistReturnedResult = result.items;
                 var avalibleMarket = result.items[i].available_markets;
                 if( avalibleMarket.indexOf('GB') > -1 ){
-	                var name = "<h4>Name:</h4> " + result.items[i].name;
-	                var link = "<h4>Spotify Listen:</h4> " + result.items[i].external_urls.spotify;
+	                var name = "<h3>Name:</h3> " + result.items[i].name;
+	                var link = "<h3>Spotify Listen:</h3> " + result.items[i].external_urls.spotify;
 	                var img = "<img src="+result.items[i].images[1].url+" height="+result.items[i].images[1].height+" width="+result.items[i].images[1].width+">"
 	               	var nameCol = name + "<br/>" + link + "<br/><br/>"+ img+ "<br/><br/><br/><br/>";
+	               	//append to the artist info div
 					$(".artistInfo").append(nameCol); 
 				};
            });
